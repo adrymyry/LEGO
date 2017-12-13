28fe5f46770ba85dd36a5eda77f3a87acb1071eb
alumno <alumno@lab27>
2017-12-13 12:45:07 +0100

    Adding files

---
+++ ServerOwncloud-91/files/config.php
@@ -0,0 +1,20 @@
+<?php
+$CONFIG = array (
+  'updatechecker' => false,
+  'instanceid' => 'oc6cdfaivqhu',
+  'passwordsalt' => '4bC9tT0DbKN7eZ/ZC4FmZI97AO3RMa',
+  'secret' => 'ab+1rcTYx6SQHqJULaVcbvwSHBG4vt+oBxTUR+nA9lD2xmS3',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.91.6',
+    1 => 'owncloud.fb.com',
+  ),
+  'datadirectory' => '/var/www/owncloud/data',
+  'overwrite.cli.url' => 'http://192.168.91.6/owncloud',
+  'dbtype' => 'sqlite3',
+  'version' => '9.0.10.2',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+  'asset-pipeline.enabled' => true,
+  'ldapIgnoreNamingRules' => false,
+);
