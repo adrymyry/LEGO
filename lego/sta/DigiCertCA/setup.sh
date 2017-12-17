41780cefdd893e1d00de5e232b7c25827117319c
Adrian Miralles <adrymyry@gmail.com>
2017-12-17 13:58:08 +0100

    Genereting directory for submission

---
+++ lego/sta/DigiCertCA/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Configuramos interfaces de red
+cp ../DigiCertCA/files/interfaces /etc/network/interfaces
+
+# Copiamos ficheros CA
+cp -r ../DigiCertCA/files/demoCA/ /home/alumno/
+cp ../DigiCertCA/files/openssl.cnf /usr/lib/ssl/
+
+# Montamos DNS Server
+apt-get update
+apt-get install -y bind9
+cp ../DigiCertCA/files/named* /etc/bind/
+cp ../DigiCertCA/files/db.digicert.com.zone /etc/bind/
+service bind9 start
+
+# Modificamos crontab para que genere crl de la CA
+crontab -l > mycron
+echo "@daily openssl ca -keyfile /home/alumno/demoCA/private/cakey.pem -gencrl -out /home/alumno/demoCA/crl/ca-crl.pem" >> mycron
+crontab mycron
+rm mycron
+
+# Instalamos Apache para servir la crl
+apt-get install -y apache2
+
+# Creamos un enlace símbolico para servir la crl con Apache
+ln -s /home/alumno/demoCA/crl/ca-crl.pem /var/www/html/ca-crl.pem
+
+# Montamos OCSP Server
+openssl ocsp -port 3333 -text -sha256\
+ -index /home/alumno/demoCA/index.txt\
+ -CA /home/alumno/demoCA/cacert.pem\
+ -rkey /home/alumno/demoCA/ocsp/ocspkey.pem\
+ -rsigner /home/alumno/demoCA/ocsp/ocspcert.pem &
---
+++ lego/topologia/facebook91.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Desactivar gestor red
+sudo service network-manager stop
+
+# Editar /etc/network/interfaces
+echo "# interfaces(5) file used by ifup(8) and ifdown(8)
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet static
+address 192.168.91.2
+netmask 255.255.255.0
+gateway 192.168.91.1
+dns-nameservers 192.168.91.2" > /etc/network/interfaces
+
+# Reiniciar interfaces
+sudo ifdown eth0
+sudo ifup eth0
+
+# Asignamos manualmente por si falla
+sudo ifconfig eth0 192.168.91.2/24
+sudo route add default gw 192.168.91.1
+
+#Montamos DNS Server
+sudo apt-get update
+sudo apt-get install -y bind9
+sudo cp ../utils/ServerDNS-91/files/* /etc/bind/
+sudo service bind9 start
