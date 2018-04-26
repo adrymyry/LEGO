37863db3db0ce61b139b9d3f816146f1d2a3d91f
Adrian Miralles <adrymyry@gmail.com>
2018-04-26 18:22:20 +0200

    access token retrieved

--- AsigSeguridad/cliente/src/main/java/es/um/lego/OAuthClientTest.java
+++ AsigSeguridad/cliente/src/main/java/es/um/lego/OAuthClientTest.java
@@ -1,4 +1,4 @@
-
+package main.java.es.um.lego;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +13,7 @@ import org.apache.oltu.oauth2.client.URLConnectionClient;
 import org.apache.oltu.oauth2.client.request.OAuthBearerClientRequest;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.client.response.GitHubTokenResponse;
+import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
 import org.apache.oltu.oauth2.client.response.OAuthResourceResponse;
 import org.apache.oltu.oauth2.common.OAuth;
 import org.apache.oltu.oauth2.common.OAuthProviderType;
@@ -30,7 +31,7 @@ public class OAuthClientTest {
 
     public static void main(String[] args) throws OAuthSystemException, IOException {
 
-        //Genear Request para el registro del cliente 
+		//Genear Request para el registro del cliente
 		System.out.println("Falta implementar la generaci�n del request para el registro del cliente");
 //		OAuthClientRequest request = OAuthClientRequest
 //				.authorizationLocation("Http://localhost:8089/AsigSeguridad/ServAutorizacion")
@@ -40,7 +41,7 @@ public class OAuthClientTest {
 
 		//Enviar Petici�n de registro al servidor de registro
 		System.out.println("Falta implementar el envio del request para el registro del cliente");
-		
+
 		//Genera el Oauth request para solicitar el Authorization code
 		OAuthClientRequest request = OAuthClientRequest
 				.authorizationLocation("http://localhost:8889/AsigSeguridad/ServAutorizacion")
@@ -50,41 +51,62 @@ public class OAuthClientTest {
 				.buildQueryMessage();
 
 		//Dos alternativas para enviar la petici�n
-		
-		//1- Desde el navegador. Para realizar la solicitud hay que pegar en el navegador la uri indicada en la consola 
-		//IMPORTANTE: Revisad que la uri est� bien generada, a veces se incluyen caracteres extra�os
-		System.out.println("Visita: " + request.getLocationUri() + "\n y copia/pega el c�digo de la uri aqui:");
-		
-		//2-Desde esta clase java
-		//URL url = new URL(request.getLocationUri());
-		//HttpURLConnection c = (HttpURLConnection)url.openConnection();
-		
-		//Procesar respuesta
-//		BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
-//		String inputLine;
-//		StringBuffer response = new StringBuffer();
-//
-//		while ((inputLine = in.readLine()) != null) {
-//			response.append(inputLine);
-//		}
-//		in.close();
-		
-		//Respuesta con el Authorization Code
-		//System.out.println(response.toString());
-		
-		System.out.println("Hay que extraer el Authorization Code de la response anterior e integrarlo en los pasos posteriores");
-		
-		//Generamos el request para pedir el access token
-		System.out.println("Falta implementar la generaci�n del request para solicitar el access token");
 
-		//Esta vez hay que realizar la petici�n sin necesidad del navegador web
-		System.out.println("Falta implementar el envio del request para la solicitud del access token");
+		//1- Desde el navegador. Para realizar la solicitud hay que pegar en el navegador la uri indicada en la consola
+		//IMPORTANTE: Revisad que la uri est� bien generada, a veces se incluyen caracteres extra�os
+		//System.out.println("Visita: " + request.getLocationUri() + "\n y copia/pega el c�digo de la uri aqui:");
+
+		//2-Desde esta clase java
+		URL url = new URL(request.getLocationUri());
+		HttpURLConnection c = (HttpURLConnection) url.openConnection();
+
+		//Procesar respuesta
+		BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//Respuesta con el Authorization Code
+		System.out.println(response.toString());
+
+		System.out.println("Hay que extraer el Authorization Code de la response anterior e integrarlo en los pasos posteriores");
+		String code = response.substring(response.lastIndexOf("=") + 1);
+		System.out.println(code);
+
+		//Generamos el request para pedir el access token
+		request = OAuthClientRequest
+				.tokenLocation("http://localhost:8889/AsigSeguridad/ServAutorizacion")
+				.setGrantType(GrantType.AUTHORIZATION_CODE)
+				.setClientId("id_prueba_registro")
+				.setClientSecret("3acb294b071c9aec86d60ae3daf32a93")
+				.setRedirectURI("http://www.as.com")
+				.setCode(code)
+				.buildBodyMessage();
 
 		//Recibimos la respuesta con el access token
-		System.out.println("Falta implementar la recepci�n del response con el access token");
+		OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
 
-		//Realizamos la petici�n de un recurso al servidor de recursos 
+		OAuthJSONAccessTokenResponse oAuthResponse = null;
+		try {
+			oAuthResponse = oAuthClient.accessToken(request);
+		} catch (OAuthProblemException e) {
+			e.printStackTrace();
+		}
+
+		//Respuesta con el Access Token
+		System.out.println("Access Token: " + oAuthResponse.getAccessToken() + ", Expires in: " + oAuthResponse
+				.getExpiresIn());
+
+		//Realizamos la peticion de un recurso al servidor de recursos
 		System.out.println("Falta implementar la comunicaci�n con el Resource Server para acceder al recurso deseado presentando el access token");
+		//Facebook profile with the retrieved accessToken
+//		OAuthClientRequest bearerClientRequest = new OAuthBearerClientRequest("https://graph.facebook.com/me").setAccessToken(oAuthResponse.getAccessToken()).buildQueryMessage();
+//		OAuthResourceResponse resourceResponse = oAuthClient.resource(bearerClientRequest, OAuth.HttpMethod.GET, OAuthResourceResponse.class);
+//		System.out.println(resourceResponse.getBody());
     }
 
 }
--- AsigSeguridad/servidor/src/main/java/es/um/lego/ServAutorizacion.java
+++ AsigSeguridad/servidor/src/main/java/es/um/lego/ServAutorizacion.java
@@ -42,9 +42,9 @@ public class ServAutorizacion extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
     	// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-		//Este método se va a encargar de proporcionar el Authorization Code 
+		//Este metodo se va a encargar de proporcionar el Authorization Code
     	System.out.println("Entra en el doGet()");
     	System.out.println("Al arrancar da una excepcion invalid_request debido a que desde el navegador se esta invocando el doGet() sin pasarle los parametros necesarios al request, no preocuparos por esto, el servidor sigue escuchando peticiones");
 		
@@ -53,52 +53,54 @@ public class ServAutorizacion extends HttpServlet {
 		
 		try {
 		
-			//Recepción del oauthRequest
+			//Recepcion del oauthRequest
 			oauthRequest = new OAuthAuthzRequest(request);
 			
-			//Dirección de respuesta
+			//Direccion de respuesta
             redirectURI = oauthRequest.getParam(OAuth.OAUTH_REDIRECT_URI);
             
-			//Aquí hay que implementar la validación de que la aplicación cliente ha sido registrada previamente
+			//Aqui hay que implementar la validacion de que la aplicacion cliente ha sido registrada previamente
         	String clientId = oauthRequest.getParam(OAuth.OAUTH_CLIENT_ID);
         	if (!clientId.equals("id_prueba_registro")) {
 				System.out.println("Invalid client");
-			}
+				response.sendError(401, "Invalid client");
+			} else {
         
-			//Se comprueba si que se está solicitando un Authorization code
-			String responseType = oauthRequest.getParam(OAuth.OAUTH_RESPONSE_TYPE);
-			if (responseType.equals(ResponseType.CODE.toString())) {
-            
-            	System.out.println("Recibe peticion de codigo");
-            	
-            	//Genera Authorization code
-                OAuthIssuerImpl oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+				//Se comprueba si que se esta solicitando un Authorization code
+				String responseType = oauthRequest.getParam(OAuth.OAUTH_RESPONSE_TYPE);
+				if (responseType.equals(ResponseType.CODE.toString())) {
 
-            	//Genera una respuesta con el Authorization Code response
-                OAuthResponse resp = OAuthASResponse.authorizationResponse(request,200)
-                        .location(redirectURI)
-                        .setCode(oauthIssuerImpl.authorizationCode())
-                        .buildQueryMessage();
-    
-                System.out.println("Genera OauthResponse");
-                
-                //Sí en la solicitud desde el cliente se ha elegido la opcion 1 (a través del navegador)
-                //Redirige la respuesta a la uri indicada en el request
-                response.sendRedirect(resp.getLocationUri());
-                System.out.println("Redirige la respuesta a la uri: "+resp.getLocationUri());
+					System.out.println("Recibe peticion de codigo");
 
-                
-            	System.out.println(resp.getResponseStatus()+"--"+resp.getLocationUri());
+					//Genera Authorization code
+					OAuthIssuerImpl oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
 
-                //Sí se ha eledigo la opcion 2
-                //Envia la respuesta
-                //response.setStatus(resp.getResponseStatus());
-    	        //PrintWriter pw = response.getWriter();
-    	        //pw.print(resp.getLocationUri());
-    	        //pw.flush();
-    	        //pw.close();
-                
-            }
+					//Genera una respuesta con el Authorization Code response
+					OAuthResponse resp = OAuthASResponse.authorizationResponse(request,200)
+							.location(redirectURI)
+							.setCode(oauthIssuerImpl.authorizationCode())
+							.buildQueryMessage();
+
+					System.out.println("Genera OauthResponse");
+
+					//Si en la solicitud desde el cliente se ha elegido la opcion 1 (a traves del navegador)
+					//Redirige la respuesta a la uri indicada en el request
+					//response.sendRedirect(resp.getLocationUri());
+					//System.out.println("Redirige la respuesta a la uri: "+resp.getLocationUri());
+
+
+					System.out.println(resp.getResponseStatus()+"--"+resp.getLocationUri());
+
+					//Si se ha eledigo la opcion 2
+					//Envia la respuesta
+					response.setStatus(resp.getResponseStatus());
+					PrintWriter pw = response.getWriter();
+					pw.print(resp.getLocationUri());
+					pw.flush();
+					pw.close();
+
+				}
+			}
 	    } catch (OAuthProblemException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -115,9 +117,61 @@ public class ServAutorizacion extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     		throws ServletException, IOException {
     	
-    	//Este método se va a encargar de proporcionar el Access token
+    	//Este metodo se va a encargar de proporcionar el Access token
     	System.out.println("Aqui se debe gestionar el Access Token");
-    	
+
+		OAuthTokenRequest oauthRequest = null;
+		String redirectURI="";
+
+		try {
+
+			//Recepcion del oauthRequest
+			oauthRequest = new OAuthTokenRequest(request);
+
+			//Direccion de respuesta
+			redirectURI = oauthRequest.getParam(OAuth.OAUTH_REDIRECT_URI);
+
+			//Aqui hay que implementar la validacion de que la aplicacion cliente ha sido registrada previamente
+			String clientId = oauthRequest.getParam(OAuth.OAUTH_CLIENT_ID);
+			if (!clientId.equals("id_prueba_registro")) {
+				System.out.println("Invalid client");
+				response.sendError(401, "Invalid client");
+				//ToDo: comprobar client secret
+			} else {
+				System.out.println("Recibe access token");
+
+				//Genera Authorization code
+				OAuthIssuerImpl oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+
+				//Genera una respuesta con el Authorization Code response
+				OAuthResponse resp = OAuthASResponse.tokenResponse(200)
+						.location(redirectURI)
+						.setTokenType(OAuth.DEFAULT_TOKEN_TYPE.toString())
+						.setAccessToken(oauthIssuerImpl.accessToken())
+						.setExpiresIn("3600")
+						.buildJSONMessage();
+
+				System.out.println("Genera OauthResponse");
+
+				System.out.println(resp.getResponseStatus()+"--"+resp.getLocationUri());
+
+				//Envia la respuesta
+				response.setStatus(resp.getResponseStatus());
+				response.setContentType(OAuth.ContentType.JSON);
+				PrintWriter pw = response.getWriter();
+				pw.print(resp.getBody());
+				pw.flush();
+				pw.close();
+			}
+
+
+		} catch (OAuthProblemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OAuthSystemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
